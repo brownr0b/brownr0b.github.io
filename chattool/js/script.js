422332b0f2a1fe0fbc14c6a4e82fc48f5749fd79
Brownrob <brownrob@iupui.edu>
2018-01-27 22:44:40 -0500

    Added files

---
+++ chattool/js/script.js
@@ -0,0 +1,9 @@
+function webdev(){
+    var url = "https://www.twitch.tv/webdev";
+    window.location = url;
+}
+
+function slap_tv(){
+    var url = "https://www.twitch.tv/slap_tv";
+    window.location = url;
+}
